814d10b1de50696aba0dffc45115b8ee5ae6ea3d
PrityKumari27 <pritykri27aug@gmail.com>
2025-05-07 07:20:09 +0000

    conversion operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -24,6 +24,52 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+
+
+// **********************OPERATIONS***********************
+let value = 3
+let negValue = -3
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2**3);
+console.log(2*2);
+console.log(2/2);
+console.log(2%3);
+
+let str1 = "Hello"
+let str2 = " Prity"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
+
+
  
 
 
