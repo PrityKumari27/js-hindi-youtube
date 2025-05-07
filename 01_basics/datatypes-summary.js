86b00afb581134b57a44114225faad3d9f791b4b
PrityKumari27 <pritykri27aug@gmail.com>
2025-05-07 16:50:54 +0000

    datatypes summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,38 @@
+// Primitive 
+
+// 7 Types: String, Number, Boolean, null, undefined, symbol, BigInt
+const score = 100
+const scoreVlaue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456736456356747633n
+
+// Reference(Non Primitive)
+
+// Array, Objects, Function
+
+const heros = ["Saktimaan", "nagraj", "doga"];
+let myObj = {
+    name: "Prity",
+    age: 20,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof bigNumber);
+console.log(typeof outsideTemp);
+console.log(typeof scoreVlaue);
+console.log(typeof myFunction);
+
+
+
