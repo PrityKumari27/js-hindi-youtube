34b1977a929521edf95b442e80534220431dab22
PrityKumari27 <pritykri27aug@gmail.com>
2025-05-09 11:51:52 +0000

    objects

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,42 @@
+// singleton
+// Object.create
+
+// object literals
+
+const mySym = Symbol("key1")
+
+
+const JsUser = {
+    name: "Prity",
+    "full name": "Prity Kumari",
+    [mySym]: "mykey1",
+    age: 18,
+    location: "Jaipur",
+    email: "prity@gmail.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+console.log(JsUser.email);
+console.log(JsUser["email"]);
+console.log(JsUser["full name"]);
+console.log(JsUser[mySym]);
+
+JsUser.email = "prity@chatgpt.com"
+// Object.freeze(JsUser)
+JsUser.email = "prity@microsoft.com"
+console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");    
+}
+
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);    
+}
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
+
+
+
+
+
