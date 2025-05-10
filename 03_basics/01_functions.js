dd74baed3c7367b14d9a22827b1c7a7f2de27086
PrityKumari27 <pritykri27aug@gmail.com>
2025-05-10 06:25:56 +0000

    functions with object

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -28,6 +28,34 @@ function loginUserMessage(username){
     return `${username} just logged in`
 }
 // console.log(loginUserMessage("Prity"));
-console.log(loginUserMessage());
+// console.log(loginUserMessage());
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+// console.log(calculateCartPrice(200, 300, 400, 50000));
+
+const user = {
+    username: "Prity",
+    price: 199
+}
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+// handleObject(user)
+handleObject({
+    username: "Sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 600, 100]
+
+function returnSecondValue(getArray){
+    return getArray[2]
+}
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 600, 10089]));
+
+
 
 
